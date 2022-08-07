573b9bdf0367c1a562d203d0c6742c42b883b07c
效辉 <dxhuii@qq.com>
2022-08-07 23:42:36 +0800

    增加header

---
+++ components/Header.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="border border-gray-100">
+    <div class="w-[1200px] mx-auto">
+      <div h-16 flex justify="between" items-center>
+        <div w-36 text="xl gray-700">
+          Cang Wang Ge
+        </div>
+        <div relative>
+          <input w-96 h-9 border="gray-200 rounded" bg="gray-100" outline="0" px-3 text="gray-600">
+          <div class="i-carbon-search right-2 top-2 absolute" text="gray-400" cursor="pointer" />
+        </div>
+        <div>menu user</div>
+      </div>
+    </div>
+  </div>
+</template>
