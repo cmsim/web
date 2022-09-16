9e996a92e296784b47d330e15fdf7fd3a580f5db
效辉 <dxhuii@qq.com>
2022-09-16 22:45:14 +0800

    增加公共字体

--- app.vue
+++ app.vue
@@ -17,6 +17,9 @@ useHead({
 
 <style>
 html, body , #__nuxt{
+  font-family: system-ui, -apple-system, BlinkMacSystemFont,
+    'SF Pro SC', 'SF Pro Display', 'PingFang SC', 'Microsoft YaHei',
+    'Lucida Grande', 'Helvetica Neue', 'Segoe UI', Helvetica, Arial, Verdana, sans-serif;
   height: 100vh;
   margin: 0;
   padding: 0;
